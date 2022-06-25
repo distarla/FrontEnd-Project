10e9113cfcfff39d5f2b70cb723cd1b12eb3a8d7
Diana Coelho <di_starla@hotmail.com>
2022-06-25 13:42:51 +0100

    Mock data and DataContext added

---
+++ src/Data/Users/usersData.js
@@ -0,0 +1,35 @@
+const usersData = [{
+    id: 1,
+    name: 'Joana Fontes',
+    username: 'Joana',
+    // JoanaFontes.1
+    password: '1dd3345b60bf1e4d917c7183973cd8e13ad3edb251be17252305a659551a001a',
+    level: 1,
+},
+{
+    id: 2,
+    name: 'João Santos',
+    username: 'Joao',
+    // JoaoSantos.2
+    password: '8e4b620a08a860f09961c8afa00652c90359d7410c720539fb6c474945f31235',
+    level: 1,
+},
+{
+    id: 3,
+    name: 'Tânia Melo',
+    username: 'Tania',
+    // TaniaMelo.3
+    password: '0a9579e061413141a6bbb72753972794aa8641d42f60bb58147af517d8892dc2',
+    level: 1,
+},
+{
+    id: 4,
+    name: 'Emília Ferreira',
+    username: 'Emilia',
+    // EmiliaFerreira.4
+    password: 'd599d5df01eedf5c0e518ed8bccbee49e81b756c22b572b53d3dc18fa316c31d',
+    level: 2,
+},
+]
+
+export default usersData;
