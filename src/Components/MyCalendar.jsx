afc1b27dfd566962915c92491d0a6c6d735ef2a5
Diana Coelho <di_starla@hotmail.com>
2022-07-03 16:30:52 +0100

    Modal add new event with date

--- src/Components/MyCalendar.jsx
+++ src/Components/MyCalendar.jsx
@@ -20,21 +20,14 @@ const MyCalendar = (props) => {
 
     const addEventButton = {
         text: 'Adicionar Evento',
-        click: function () {
-            var dateStr = prompt('Enter a date in YYYY-MM-DD format');
-            var date = new Date(dateStr + 'T00:00:00'); // will be in local time
+        click: ()=>props.addEventButtonClick()
             
-            if (!isNaN(date.valueOf())) { // valid?
-                calendar.addEvent({
-                    title: 'dynamic event',
-                    start: date,
-                    allDay: true
-                });
-                alert('Great. Now, update your database...');
-            } else {
-                alert('Invalid date.');
-            }
-        }
+            // if (!isNaN(date.valueOf())) { // valid?
+            //     calendar.addEvent({
+            //         title: 'dynamic event',
+            //         start: date,
+            //         allDay: true
+            //     });
     }
 
     useEffect(() => {
