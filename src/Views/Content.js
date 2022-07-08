c0ce0bc50b2b1f6c9eca2bc7f83768686e3fd3a8
Diana Coelho <di_starla@hotmail.com>
2022-07-08 16:38:40 +0100

    Authenticated Route

--- src/Views/Content.js
+++ src/Views/Content.js
@@ -1,20 +1,36 @@
-import React from 'react';
-import { Routes, Route } from 'react-router-dom'
+import React, {useContext} from 'react';
+import { Routes, Route, Navigate } from 'react-router-dom'
 import LoginPage from './LoginPage';
 import Home from './Home'
 import NotFound from './NotFound';
 
+import DataContext from '../Data/Users/dataContext';
 
+const Content = () => {
 
+    const { state, setState } = useContext(DataContext);
 
-const Content = () => (
-    <main className='Content'>
-        <Routes>
-            <Route path="/" element={<LoginPage />} />
-            <Route path="/home" element={<Home />} />
-            <Route path="*" element={<NotFound />} />
-        </Routes>
-    </main>
-)
+    const redirect = () => {
+        if (state.loggedIn) {
+            return (
+                <Home />
+            )
+        } else {
+            return (
+                <Navigate to="/" replace={true} />
+            )
+        }
+    }
+
+    return(
+        <main className='Content'>
+            <Routes>
+                <Route path="/" element={<LoginPage />} />
+                <Route path="/home" element={redirect()} />
+                <Route path="*" element={<NotFound />} />
+            </Routes>
+        </main>
+    )
+}
 
 export default Content;
